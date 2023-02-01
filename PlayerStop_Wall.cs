33d1679da94776fee3f7c2435291904d777d8bd5
aqswdefr1234 <108164564+aqswdefr1234@users.noreply.github.com>
2023-02-01 13:18:28 +0900

    Add files via upload

---
+++ PlayerStop_Wall.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStop_Wall : MonoBehaviour
+{
+    private int trigger = 0;
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("콜리전enter 작동중");
+        if(trigger == 0)
+        {
+            if (other.collider.CompareTag("Player")) //트리거에서는 other.tga == 로 간단하게 태그 사용가능
+            {
+                other.transform.parent.GetComponent<PlayerMoveAndCamera>().speedSetting = 1f;
+                trigger = 1;
+            }
+        }
+    }
+    void OnCollisionExit(Collision other)
+    {
+        Debug.Log("콜리전exit 작동중");
+        if (trigger == 1)
+        {
+            if (other.collider.CompareTag("Player"))
+            {
+                other.transform.parent.GetComponent<PlayerMoveAndCamera>().speedSetting = 10f;
+                trigger = 0;
+            }
+        }
+    }
+}
